5241c077266b213f4b8b5da664af3d1dbcdd5a0d
yoni13 <yoni@nicewhite.xyz>
2023-12-14 22:29:45 +0800

    add vec9

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Classrum-DataToModel
+ # Classrum-DataToModel
+
+ ## introduction  
+This is a part of the Classrum project,a product that aims to provide a open-source althernativw to 1Campusm,Google Classroom.  
 
--- main.py
+++ main.py
@@ -26,31 +26,32 @@ vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(data)
 
 # 切割數據集
-X_train, X_test, y_train, y_test = train_test_split(X, labels, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, labels, test_size=0.8, random_state=42)
 
 # 選擇機器學習算法，這裡使用多類SVM
 clf = SVC(kernel='linear', C=1)
 clf.fit(X_train, y_train)
-
-# 預測未知的科目
-new_data = ["卷"]
-new_data_vectorized = vectorizer.transform(new_data)
-predicted_subject = clf.predict(new_data_vectorized)
-print(predicted_subject)
-
-
-
-
-
-
-
-
 
 # 評估模型
 y_pred = clf.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print("模型準確度:", accuracy)
 
+
 # 部署模型，可以保存模型以供以後使用
-from joblib import dump, load
-dump(clf, 'subject_recognition_model.joblib')
+if input('保存模型嗎？請回y \n') == 'y':
+    from joblib import dump, load
+    dump(clf, 'subject_recognition_model.joblib')
+    dump(vectorizer, 'subject_reconition_vec.joblib')
+
+# 預測未知的科目
+while True:
+    new_data = [input('預測科目模型測試:')]
+    new_data_vectorized = vectorizer.transform(new_data)
+    predicted_subject = clf.predict(new_data_vectorized)
+    print(predicted_subject)
+
+
+
+
+
