6105659f32f22721b80859551487cdc99026cc1d
yoni13 <yoni@nicewhite.xyz>
2024-03-23 14:28:28 +0800

    -y

--- setupdocker.sh
+++ setupdocker.sh
@@ -12,6 +12,6 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 docker run --name some-mongo -d mongo:tag
