50f52f9ba5f0b4a90d4c9e4aa22d93dcf7b089b8
yoni13 <yoni@nicewhite.xyz>
2024-03-23 14:33:23 +0800

    sudo ahaim!

--- setupdocker.sh
+++ setupdocker.sh
@@ -14,4 +14,4 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-docker run --name some-mongo -d mongo:tag
+sudo docker run --name some-mongo -d mongo:tag
